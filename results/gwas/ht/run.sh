68a5ab99f6ec536f4ebb7bdc7031d55660ac6619
hclimente <hclimente@gmail.com>
2018-11-19 02:33:40 +0900

    added run scripts for some diseases

---
+++ results/gwas/ht/run.sh
@@ -0,0 +1,1 @@
+../../../scripts/select_features.nf --input EGAD00000000009.ped --map1 EGAD00000000009.map --ped2 EGAD00000000001.ped --map2 EGAD00000000001.map -resume -with-trace
---
+++ results/gwas/t1d/run.sh
@@ -0,0 +1,1 @@
+../../../scripts/select_features.nf --input EGAD00000000008.ped --map1  EGAD00000000008.map --ped2  EGAD00000000001.ped --map2 EGAD00000000001.map -resume -with-trace
