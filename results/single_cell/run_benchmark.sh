f4417e60739b4f4378a3b63a80bf37c5eb9c56bf
hclimente <hclimente@gmail.com>
2018-11-21 15:32:39 +0900

    test low-rank work around

--- results/single_cell/run_benchmark.sh
+++ results/single_cell/run_benchmark.sh
@@ -3,6 +3,6 @@ do
     path="${HOME}/data/single_cell/${line[1]}/"
     nextflow run ../../scripts/benchmark_real.nf --input "${path}${line[2]}" \
 --metadata "${path}${line[3]}" --col_feats "${line[4]}" --col_id "${line[5]}" \
---col_y "${line[6]}" --causal 10,20,30,40,50,80,100 --perms 100 --B 5,10,20 -profile cluster -resume "$@"
+--col_y "${line[6]}" --causal 10,20,30,40,50 --perms 100 --B 5,10,20 -profile cluster -resume "$@"
     mv "${line[2]%.*}_prediction.tsv" "${line[1]}_prediction.tsv"
 done < datasets.tsv
