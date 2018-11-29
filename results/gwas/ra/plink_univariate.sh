ba7eb5f01cc0405cb95031b6c8f4fc0face7cc41
hclimente <hclimente@gmail.com>
2018-11-29 23:59:37 +0100

    several updates

---
+++ results/gwas/ra/plink_univariate.sh
@@ -0,0 +1,1 @@
+gwas_univariate --ped1 EGAD00000000007.ped --map1  EGAD00000000007.map --ped2  EGAD00000000001.ped --map2 EGAD00000000001.map -with-trace -with-docker hclimente/gwas-tools -resume
---
+++ results/gwas/t1d/plink_univariate.sh
@@ -0,0 +1,1 @@
+gwas_univariate --ped1 EGAD00000000008.ped --map1  EGAD00000000008.map --ped2  EGAD00000000001.ped --map2 EGAD00000000001.map -with-trace -with-docker hclimente/gwas-tools -resume
