4a5783bcd078b70112631586efd157edd9d7e2d0
hclimente <hclimente@gmail.com>
2018-12-14 17:14:01 +0900

    updaed stability benchmark

--- results/stability/run.sh
+++ results/stability/run.sh
@@ -1,2 +1,2 @@
 # manually add list of number of permutations to benchmark.nf
-../../scripts/benchmark.nf "$@" --n 10000 --d 10000 --data_generation random -with-trace -resume -profile cluster
+../../scripts/benchmark.nf "$@" --n 1000 --d 2500 --data_generation random -with-trace -resume -profile cluster
