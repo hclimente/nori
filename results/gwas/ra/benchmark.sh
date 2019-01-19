587420a9b37410ada7870c2497ad9b3d5f638a20
hclimente <hclimente@gmail.com>
2019-01-19 21:35:48 +0900

    updated results without sexual chhromosomes 1/2

--- results/gwas/ra/benchmark.sh
+++ results/gwas/ra/benchmark.sh
@@ -1,1 +1,1 @@
-nextflow run ../../../scripts/benchmark_real.nf  --input ra.bed --bim1 ra.bim --fam1 ra.fam --bed2 controls.bed --bim2 controls.bim --fam2 controls.fam --B 20 --perms 5 -profile cluster -resume "$@"
+nextflow run ../../../scripts/benchmark_real.nf  --input ra.bed --bim1 ra.bim --fam1 ra.fam --bed2 controls.bed --bim2 controls.bim --fam2 controls.fam --causal 10,20 --B 20 --perms 5 -profile cluster -resume "$@"
