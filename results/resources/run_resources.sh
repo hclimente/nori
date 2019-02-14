529a5720fe4fa8ff2b940b81b4c54f06d6f01b38
hclimente <hclimente@gmail.com>
2019-02-14 18:35:28 +0900

    rename relevant trace files

--- results/resources/run_resources.sh
+++ results/resources/run_resources.sh
@@ -1,1 +1,2 @@
 ../../scripts/benchmark.nf "$@" --M '3, n_jobs=1' --n 1000 --d 2500 --data_generation random -with-trace -resume -profile cluster
+mv trace.txt resources_trace.txt
--- results/resources/run_stability.sh
+++ results/resources/run_stability.sh
@@ -1,2 +1,5 @@
 # manually add list of number of permutations to benchmark.nf
 ../../scripts/benchmark.nf "$@" --n 1000 --d 2500 --data_generation random -with-trace -resume -profile cluster
+
+mv trace.txt stability_trace.txt
+mv random_feature_selection.txt stability_feature_selection.txt
