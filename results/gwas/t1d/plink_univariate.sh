a4a9d0fe867dc9bc7fa8e8d34bba9498485e4a27
hclimente <hclimente@gmail.com>
2019-03-21 16:13:49 +0100

    updated results

--- results/gwas/t1d/plink_univariate.sh
+++ results/gwas/t1d/plink_univariate.sh
@@ -1,4 +1,4 @@
 merge_cc --file1 controls.bed --file2 t1d.bed -with-docker hclimente/gwas-tools
-plink --bfile merged --chr 1-22 --make-bed -out merged
+plink --bfile merged --chr 1-22 --maf 0.05 --mind 0.1 --geno 0.1 --hwe 0.001 --make-bed -out merged
 plink --bfile merged --model fisher
 sed 's/^ \+//' plink.model | sed 's/ \+/\t/g' | sed 's/\t$//' >univariate_models.tsv
