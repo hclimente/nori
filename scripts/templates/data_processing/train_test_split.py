5712227755df855c6917e53181a459abb6d2ab2a
hclimente <hclimente@gmail.com>
2020-09-21 08:25:55 +0900

    balance datasets after train test split

--- scripts/templates/data_processing/train_test_split.py
+++ scripts/templates/data_processing/train_test_split.py
@@ -14,6 +14,7 @@ Output files:
 
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
 
 np.random.seed(${SEED})
 x = np.load("${X}")
@@ -25,6 +26,20 @@ x = x[perm,:]
 y = y[perm]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = ${SPLIT})
+
+# balance the classes
+unique_elements, counts_elements = np.unique(y_train, return_counts=True)
+idx_minority = (y_train == unique_elements[np.argmin(counts_elements)])
+
+x_minority, y_minority = resample(x_train[idx_minority,:],
+        y_train[idx_minority],
+        replace = True,
+        n_samples = np.max(counts_elements),
+        random_state = 42)
+x_majority = x_train[np.logical_not(idx_minority),:]
+y_majority = y_train[np.logical_not(idx_minority)]
+x_train = np.concatenate((x_minority, x_majority))
+y_train = np.concatenate((y_minority, y_majority))
 
 np.save("x_train.npy", x_train)
 np.save("x_test.npy", x_test)
